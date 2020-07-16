eb39e7798fee8833bd8936332c756f4986c0eb7c
e <578755236@qq.com>
2020-07-16 23:49:22 +0800

    vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: '大白菜的博客',
+  description: '专注 React 技术栈分享，从React基础到深入原理',
+  themeConfig: {
+    nav: [{ text: "主页", link: "/" },
+    { text: "前端", link: "/webframe/" },
+    { text: "React", link: "/react/" },
+    { text: "Vue", link: "/vue/" },
+    { text: "JavaScript", link: "/javaScript/" },
+    { text: "面试问题", link: "/interview/" }
+    ],
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+---
+home: true
+heroImage: /home.jpg
+actionText: Get Started →
+actionLink: /node/
+features:
+- title: day day up
+  details: 记录每一天的进步, 一分耕耘，一分收获.
+- title: 记录日常学习
+  details: 专注 React 技术栈分享，从React基础到深入原理，祝您成为优秀的资深前端工程师
+- title: koala
+  details: 一个有趣的且乐于分享的人。座右铭：今天未完成的，明天更不会完成。
+footer: MIT Licensed | Copyright © 2016-present Evan You
